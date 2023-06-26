1fe59b1b83c99df0117e01c028f696918d989cc5
aliabgithub <ali.arjomandbigdeli@stonybrook.edu>
2023-06-26 18:26:24 -0400

    add venv path to path

--- vnncomp_scripts/run_instance.sh
+++ vnncomp_scripts/run_instance.sh
@@ -28,6 +28,7 @@ export OPENBLAS_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
 # run the tool to produce the results file
+export PATH="/root/.local/share/virtualenvs/toolkit-Z_JSe2nD/bin:$PATH"
 cd $DIR
 pipenv run python -m nnenum.nnenum -o "$ONNX_FILE" -v "$VNNLIB_FILE" -t "$TIMEOUT" -f "$RESULTS_FILE" -s "$CATEGORY"
 # python3 -m nnenum.nnenum -o "$ONNX_FILE" -v "$VNNLIB_FILE" -t "$TIMEOUT" -f "$RESULTS_FILE" -s "$CATEGORY"
